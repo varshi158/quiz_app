39d15395210751051f1674839690470cd85007da
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-03-04 18:20:52 +0530

    answer button using map instead of hard coding

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,22 +14,33 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+
+    // accessing "questions" list 
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "The question...",
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(
             height: 30,
           ),
-          AnswerButton(
-            answerText: "hello",
-            onTap: () {},
-          ),
+          // "answers" is an list 
+          // "map" allows you to convert values in a list into other values
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {})
+          }),
+
+          // AnswerButton(
+          //   answerText: currentQuestion.answers[0],
+          //   onTap: () {},
+          // ),
+        
         ],
       ),
     );
