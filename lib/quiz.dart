423832afa3f5870daa64ba5aaf79acaa20a17967
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-03-04 14:41:06 +0530

    another approach to connect two screens

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -14,25 +14,36 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget ? activeScreen;
+  // Widget ? activeScreen;
 
-  @override
-  void initState() {
-    super.initState();
-    activeScreen = StartScreen(switchScreen);
-    
-  }
+  var activeScreen = "start-screen";
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   activeScreen = StartScreen(switchScreen);
+  // }
 
   // needs to be connected to the button on StartScreen
   // startscreen needs access to this function
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      //activeScreen = const QuestionsScreen();
+      activeScreen = "questions-screen";
     });
   }
 
   @override
   Widget build(BuildContext context) {
+
+    // setting content conditionally
+    Widget screenWidget = StartScreen(switchScreen);
+            
+    if (activeScreen == "questions-screen") {
+        screenWidget = const QuestionsScreen();
+    }
+    
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -46,7 +57,11 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          //child: activeScreen,
+          // child: activeScreen == "start-screen"
+          //     ? StartScreen(switchScreen)
+          //     : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
