639a4a865d4481b69b4b430b8973535d6f6f7e48
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-03-05 07:28:24 +0530

    make buttons take up similar width

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -14,34 +14,39 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-
-    // accessing "questions" list 
+    // accessing "questions" list
     final currentQuestion = questions[0];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          // "answers" is an list 
-          // "map" allows you to convert values in a list into other values
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onTap: () {})
-          }),
-
-          // AnswerButton(
-          //   answerText: currentQuestion.answers[0],
-          //   onTap: () {},
-          // ),
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            // "answers" is an list
+            // "map" allows you to convert values in a list into other values
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
         
-        ],
+            // AnswerButton(
+            //   answerText: currentQuestion.answers[0],
+            //   onTap: () {},
+            // ),
+          ],
+        ),
       ),
     );
   }
