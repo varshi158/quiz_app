13ae18efb09b9144d1e51ff90c0f6df53aa0ac47
Varshika Dhevanayagam <varshika.20210977@iit.ac.lk>
2024-03-05 07:45:07 +0530

    center answer texts

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -31,7 +31,10 @@ class AnswerButton extends StatelessWidget {
         foregroundColor: Colors.white,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
